b88565e7781d775323ca8c17b0207f3146a3bc74
FelixBanan <solgier7@yandex.ru>
2018-08-30 19:00:24 +0300

    Калькулятор для простых уравнений

---
+++ EasyPas/CalcXY.pas
@@ -0,0 +1,30 @@
+﻿Program CalcXY;
+
+var
+  x, y: real;
+
+begin
+
+  writeln('Введите значение x');
+  read(x);
+
+  if (x < 0) then
+    begin
+    
+      y := x*x
+    
+    end
+  else if x=0 then
+    begin
+      y := x-1
+    end
+  else
+    begin
+    y := 2*x
+    end;
+  //Конец блока IF
+    
+  writeln('Решение уравнения: y = ',y);
+   
+
+end.
