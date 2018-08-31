651ec254dc48d6ac687492b00e5e313b2b1593b6
FelixBanan <solgier7@yandex.ru>
2018-08-31 18:31:53 +0300

    Простейший калькулятор

---
+++ EasyPas/Calc.pas
@@ -0,0 +1,24 @@
+﻿Program Calc;
+
+var
+  a,b: integer;
+
+begin
+
+  writeln('Введите значение a');
+  read(a);
+  writeln('Введите значение b');
+  read(b);
+  
+  writeln('a+b=', a+b);
+  writeln('a-b=', a-b);
+  writeln('a*b=', a*b);
+  writeln('a/b=', a/b);
+
+  writeln('a^2=', a*a);
+  writeln('корень из a=', sqrt(a));
+  
+  writeln('Целое от деления a на b=', a div b);
+  writeln('Остаток от деления a на b=', a mod b);
+
+end.
