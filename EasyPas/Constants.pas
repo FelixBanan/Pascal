e94961e87e3fab20d987f696220ce4028d1635bd
FelixBanan <solgier7@yandex.ru>
2018-08-31 18:31:41 +0300

    Примеры работ констант, циклов и условий.

---
+++ EasyPas/Constants.pas
@@ -0,0 +1,18 @@
+﻿Program First;
+
+const
+  PI = 3.14;
+  FELIX_YEARS = 14;
+  H = 'Hello User';
+  VSK = '!';
+  
+var 
+  a,b: real;
+  user: string;
+  func: char;
+
+begin
+
+  writeln(H,VSK,' ','Число PI=',PI,'. Тебе ', FELIX_YEARS, ' лет.');
+
+end.
---
+++ EasyPas/IFS.pas
@@ -0,0 +1,25 @@
+﻿Program IFS;
+
+var
+
+  a,b: integer;
+
+begin
+
+  a := 12;
+  
+  b := 12;
+
+  if (a = b) then
+    begin
+      a := 13;
+    end
+  else
+    begin
+      a := 14;
+    end;
+  //Конец Блока IF
+   
+   writeln(a);
+   
+end.
