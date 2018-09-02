5b7befc911d5a12f07159bd49f9b85de204b74d8
FelixBanan <solgier7@yandex.ru>
2018-09-02 22:15:09 +0300

    Таймер

---
+++ Other/Timer.pas
@@ -0,0 +1,20 @@
+﻿uses crt ;
+
+var
+  i, sec, min: integer;
+
+begin
+  write('Введите количество минут: '); readln(min);
+  
+  for i := 0 to min - 1 do
+  begin
+    for sec := 0 to 60 do
+    begin
+      writeln(i, ' : ', sec);
+      Delay(1000);
+      clrscr;
+      
+      if KeyPressed then exit;
+    end;
+  end;
+end.
