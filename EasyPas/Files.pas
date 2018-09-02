b379d612a7a49872e5668e78567b09d90dfd98e1
FelixBanan <solgier7@yandex.ru>
2018-09-02 22:53:56 +0300

    Пример работы с файлами

---
+++ EasyPas/Files.pas
@@ -0,0 +1,58 @@
+﻿uses crt;
+
+{
+
+  Пример работы с файлами.
+
+}
+
+const
+  FILENAME = 'file.txt';
+
+var
+  f: text;
+  info: string;
+  a: integer;
+begin
+  
+  
+  writeln('Введите индентификатор программы(1 запись, 2 чтение)');
+  readln(a);
+  
+  case a of
+  1:
+    begin
+    
+      writeln('Введите информацию для записи в файл:');
+      readln(info);
+      
+      assign(f,FILENAME);
+      rewrite(f);
+      
+      write(f,info);
+      close(f);
+      
+      writeln('Информация успешна записана');
+    end;
+   2:
+    begin
+      
+      assign(f,FILENAME);
+      reset(f);
+      
+      read(f,info);
+      close(f);
+      
+      writeln(info);
+      
+    end;
+    
+   else
+   
+    writeln('Такого индентификатора нет.');
+   
+   end;
+  
+  
+
+end.
