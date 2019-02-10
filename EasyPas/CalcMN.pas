976454cca4fe882dca492e80e69ae8c1eb9b17e0
FelixBanan <solgier7@yandex.ru>
2019-02-10 15:31:09 +0300

    Алгоритм для вывода значения MN

---
+++ EasyPas/CalcMN.pas
@@ -0,0 +1,25 @@
+﻿program calcmn;
+
+uses crt;
+
+var
+  m, n: integer;
+
+begin
+
+  write('Введите значение m:');
+  read(m);
+  
+  write('Введите значение n:');
+  read(n);
+  
+  repeat
+    begin
+      m := m - 2;
+      n := n * 2;
+    end;
+  until m < 6;
+  
+  writeln('n = ', n,'; m = ', m, ';');
+  
+end.
