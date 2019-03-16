33ebf1428d6bbd68421a4d1f5f23eaafd8e10c85
FelixBanan <solgier7@yandex.ru>
2019-03-16 20:50:17 +0300

    Лотерейные билеты и 9 номер

---
+++ EasyPas/ex9.pas
@@ -0,0 +1,21 @@
+﻿program Ex9;
+
+var
+  a, a2, b, c: integer;
+
+begin
+  write('Введите значение c: ');
+  readln(c);
+  
+  b := (c mod 10) * 10;
+  b := b + (c div 10);
+  
+  a := c div 10 + c mod 10; 
+  a2 := (c div 10) * (c mod 10); 
+  
+  writeln('Задание а) Ответ:'); 
+  writeln('Сумма цифр числа: ', a); 
+  writeln('Произведение цифр числа: ', a2); 
+  writeln('Задание б) Ответ: ', b); 
+  
+end.
---
+++ EasyPas/mvp.pas
@@ -0,0 +1,16 @@
+﻿program mvp;
+
+begin
+  // Для PascalABC.NET
+  writeln('PascalABC.NET');
+  writeln('Для молодёжи: ', random(1, 101));
+  writeln('Для взрослых: ', random(101, 200));
+  writeln('Для пенсионеров: ', random(201, 250));
+  
+  // Для PascalABC
+  writeln('PascalABC');
+  writeln('Для молодёжи: ', random(100) + 1);
+  writeln('Для взрослых: ', random(100) + 101);
+  writeln('Для пенсионеров: ', random(50) + 201);
+  
+end.
