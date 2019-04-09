7e3fcbd1662d4db46c39fb78048faafd09eef9d3
FelixBanan <solgier7@yandex.ru>
2019-04-09 22:41:07 +0300

    Калькуляторы Лехи

---
+++ EasyPas/CalcCXC.pas
@@ -0,0 +1,33 @@
+﻿program cxc;
+
+uses crt;
+
+var
+  x, y: integer;
+
+begin
+  write('Введите значение x >>>'); 
+  read(x); 
+  
+  if x < 0 then 
+  begin
+    y := sqr(x); 
+    writeln('x^2: ', y); 
+  end 
+  else 
+  begin
+    if x = 0 then 
+    begin
+      y := x * 1; 
+      writeln('x - 1: ', y); 
+    end 
+    else 
+    begin
+      y := 2 * x; 
+      writeln('2 * x: ', y); 
+    end; 
+  end; 
+  
+  writeln('Значение y: ', y); 
+  
+end.
---
+++ Other/CalcByLeha.pas
@@ -0,0 +1,63 @@
+﻿program calculator;
+
+uses crt;
+
+var
+  numbs: array[1..3] of real; 
+  mark: char;
+
+begin
+  
+  writeln('Татарский калькулятор 3000 (С) Алексей Жмыхайлов'); 
+  while true do 
+  begin
+    write('Введите первую переменную »> '); 
+    readln(numbs[1]); 
+    write('Введите знак арефметического выражения »> '); 
+    readln(mark); 
+    if mark <> '^' then 
+    begin
+      write('Введите вторую переменную »> '); 
+      readln(numbs[2]); 
+    end;
+    if mark in ['+', '-', '*', '/', '^'] then 
+    begin
+      case mark of 
+        '+':
+          begin
+            numbs[3] := numbs[1] + numbs[2]; 
+            writeln(numbs[3]);
+          end; 
+        '-':
+          begin
+            numbs[3] := numbs[1] - numbs[2]; 
+            writeln(numbs[3]);
+          end; 
+        '*':
+          begin
+            numbs[3] := numbs[1] * numbs[2];
+            writeln(numbs[3]);
+          end; 
+        '/':
+          begin
+            if numbs[2] <> 0 then 
+            begin
+              numbs[3] := numbs[1] / numbs[2];
+              writeln(numbs[3]);
+            end
+            else
+            begin
+              writeln('Деление на ноль невозможно'); 
+            end;
+          end; 
+        '^':
+          begin
+            numbs[3] := sqr(numbs[1]); 
+            writeln(numbs[3]);
+          end;
+      end;
+    end 
+    else 
+      write('Неверно введен арифметический знак'); 
+  end; 
+end.
