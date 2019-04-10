4cc51c381f254b3e28fe9b3cd67f8ddf9804647b
FelixBanan <solgier7@yandex.ru>
2019-04-11 00:49:08 +0300

    Пример работы условий

---
+++ EasyPas/ex13.pas
@@ -0,0 +1,21 @@
+﻿program n_13;
+
+uses crt;
+
+var
+  x, y: integer;
+
+begin
+  
+  write('Введите значение x >> ');
+  readln(x);
+  
+  if(x < 0) then
+    y := -1
+  else if (x = 0) then
+    y := 0
+  else if (x > 0) then
+    y := 1;
+  
+  writeln('Значение y: ', y);
+end.
