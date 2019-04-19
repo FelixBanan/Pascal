011b62b5d8b4a2f413b2748a5f8a48696e670695
FelixBanan <FelixASD@ya.ru>
2019-04-19 21:57:18 +0300

    Create p3.5.1ex3.pas

---
+++ EasyPas/p3.5.1ex3.pas
@@ -0,0 +1,21 @@
+﻿Program p351ex3;
+
+uses crt;
+
+var k, f, n: integer;
+
+begin
+  
+  write('Введите n >>');
+  readln(n);
+  
+  f:=1;
+  
+  while k<n do
+    begin
+      k:=k+1;
+      f:=f*k;
+    end;
+
+  writeln(n, '!=', f);
+end.
