34b7bf0f0ecd756484f7fe59ecab2c4aeabc6af2
FelixBanan <FelixASD@ya.ru>
2019-11-15 00:14:50 +0300

    Create ex13_class9.pas

---
+++ EasyPas/ex13_class9.pas
@@ -0,0 +1,26 @@
+﻿program n13;
+uses crt;
+
+const
+  batch = 50;
+  procent = 1.2;
+
+var
+  a, batch_sum, no_batch: real;
+  k: integer;
+
+begin
+  
+  writeln('Подсчёт выручки продавца газет');
+  writeln();
+  write('Введите стоимость газет: ');
+  readln(a);
+  write('Введите количество газет: ');
+  readln(k);
+  
+  batch_sum := batch * a; // Стоимость первых 50 газет
+  no_batch := (k - batch) * procent * a; // Кол-во газет без тех которые без процентные
+  
+  writeln('Выручка: ', batch_sum + no_batch, ' руб.');
+  
+end.
