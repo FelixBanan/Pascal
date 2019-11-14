ec68357b3f59f8d8f7c78482fc24645bcf2f7fea
FelixBanan <FelixASD@ya.ru>
2019-11-14 23:56:15 +0300

    Create ex12.pas

---
+++ EasyPas/ex12.pas
@@ -0,0 +1,29 @@
+﻿program n13;
+uses crt;
+
+const
+  economy = 120;
+  business = 40;
+
+var
+  a, b: integer;
+  gain, x, y: real;
+
+begin
+  
+  writeln('Подсчёт выручки с одного полета аэробуса');
+  writeln();
+  write('Введите стоимость билетов в бизнес-классе: ');
+  readln(x);
+  write('Введите количество непроданных билетов бизнес-класса: ');
+  readln(a);
+  write('Введите количество непроданных билетов эконом-класса: ');
+  readln(b);
+  
+  y := x / 2; // Стоимость билета эконом класса
+  
+  gain := (business - a) * x + (economy - b) * y;
+  
+  writeln('Выручка: ', gain, ' руб.');
+  
+end.
